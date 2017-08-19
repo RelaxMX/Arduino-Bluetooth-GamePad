6a920947095dfcedbd2ecb173bb056ab86a52f44
RelaxMX <31166191+RelaxMX@users.noreply.github.com>
2017-08-19 16:14:43 -0500

    extend compatibility

--- Joy.ino
+++ Joy.ino
@@ -1,12 +1,13 @@
+// ATtyny2313 pins   {PA1,PA0,PD2,PD3,PD4,PD5,PD6,PB0,PB1,PB2,PB3,PB4,PB5,PB6,PB7};
+static const uint8_t pins[] =  {2,3,4,5,6,7,8,9,10,11,12,A0,A1,A2,A3}; // Arduino Uno|Mini Pro, Button pins
 
-static const uint8_t pins[] = {2,3,4,5,6,7,8,9,10,11,12,A0,A1,A2,A3};
 
 typedef struct
  {
-     uint32_t st;
-     uint32_t nd;
-     uint32_t st_o;
-     uint32_t nd_o;
+     uint8_t st;
+     uint8_t nd;
+     uint8_t st_o;
+     uint8_t nd_o;
  }  gamepad;
 
 gamepad gp;
@@ -16,7 +17,7 @@ void setup() {
   gp.nd = 0;
   gp.st_o = 0;
   gp.nd_o = 0;
-  for (int p = 0; p < 15; p++) {
+  for (uint8_t p = 0; p < sizeof(pins); p++) {
      pinMode(  pins[p] , INPUT_PULLUP);
      digitalWrite(  pins[p] , HIGH);
   }
@@ -26,7 +27,7 @@ void setup() {
 
 void loop() {
 
-  for(int p = 0; p <15; p++) {
+  for(uint8_t p = 0; p < sizeof(pins); p++) {
     if (digitalRead(  pins[p] ) == LOW) {
       press( p + 1 );
     }
@@ -57,18 +58,18 @@ void JoyState(byte ST_BTN, byte ND_BTN) {
 void press(uint8_t b){ 
   if (b > 7) { // second byte
     b = b - 7;
-    gp.nd_o |= (uint32_t)1 << (b - 1); 
+    gp.nd_o |= (int)1 << (b - 1); 
   }
   else { // fisrt byte
-    gp.st_o |= (uint32_t)1 << (b - 1); 
+    gp.st_o |= (int)1 << (b - 1); 
   }
 }
 void release(uint8_t b){ 
   if (b > 7) { // second byte
     b = b - 7;
-    gp.nd_o &= ~((uint32_t)1 << (b - 1)); 
+    gp.nd_o &= ~((uint8_t)1 << (b - 1)); 
   }
   else { // first byte
-    gp.st_o &= ~((uint32_t)1 << (b - 1)); 
+    gp.st_o &= ~((uint8_t)1 << (b - 1)); 
   }
 }
