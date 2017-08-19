c84a4c8ea7aa0b5a18e8480d0ed7e913fa0e429c
RelaxMX <31166191+RelaxMX@users.noreply.github.com>
2017-08-19 14:09:18 -0500

    use struct

--- Joy.ino
+++ Joy.ino
@@ -1,20 +1,27 @@
-uint32_t st;
-uint32_t nd;
-uint32_t st_o;
-uint32_t nd_o;
-static const uint8_t pins[] = {2,3,4,5,6,7,8,9,10,11,12,A0,A1,A2,A3}; // button pins
+
+static const uint8_t pins[] = {2,3,4,5,6,7,8,9,10,11,12,A0,A1,A2,A3};
+
+typedef struct
+ {
+     uint32_t st;
+     uint32_t nd;
+     uint32_t st_o;
+     uint32_t nd_o;
+ }  gamepad;
+
+gamepad gp;
 
 void setup() {
-  st = 0;
-  nd = 0;
-  st_o = 0;
-  nd_o = 0;
+  gp.st = 0;
+  gp.nd = 0;
+  gp.st_o = 0;
+  gp.nd_o = 0;
   for (int p = 0; p < 15; p++) {
      pinMode(  pins[p] , INPUT_PULLUP);
      digitalWrite(  pins[p] , HIGH);
   }
   Serial.begin(115200);
-  delay(500);
+  delay(500); //Delay
 }
 
 void loop() {
@@ -28,10 +35,10 @@ void loop() {
     }
   }
  
-  if ( st_o != st || nd_o != nd    ) {
-    st = st_o;
-    nd = nd_o;
-    JoyState( (char) st , (char) nd );
+  if ( gp.st_o != gp.st || gp.nd_o != gp.nd    ) {
+    gp.st = gp.st_o;
+    gp.nd = gp.nd_o;
+    JoyState( (char) gp.st , (char) gp.nd );
   }
   delay(20);
 }
@@ -50,19 +57,18 @@ void JoyState(byte ST_BTN, byte ND_BTN) {
 void press(uint8_t b){ 
   if (b > 7) { // second byte
     b = b - 7;
-    nd_o |= (uint32_t)1 << (b - 1); 
+    gp.nd_o |= (uint32_t)1 << (b - 1); 
   }
   else { // fisrt byte
-    st_o |= (uint32_t)1 << (b - 1); 
+    gp.st_o |= (uint32_t)1 << (b - 1); 
   }
 }
 void release(uint8_t b){ 
   if (b > 7) { // second byte
     b = b - 7;
-    nd_o &= ~((uint32_t)1 << (b - 1)); 
+    gp.nd_o &= ~((uint32_t)1 << (b - 1)); 
   }
   else { // first byte
-    st_o &= ~((uint32_t)1 << (b - 1)); 
+    gp.st_o &= ~((uint32_t)1 << (b - 1)); 
   }
 }
-
